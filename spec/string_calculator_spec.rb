76338698926b9e444fd234a0da7461a548b2d978
ssaha777 <ssaha@kreeti.com>
2024-05-21 20:18:58 +0530

    Initial commit

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
+end
